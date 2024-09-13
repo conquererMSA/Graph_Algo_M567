bbba1b466490379a759bc17e82cdf2e8e8992ec4
asus test <msa-99@outlook.com>
2024-09-13 15:34:25 +0600

    floyed warshel cycle detection

--- floyedWarshell.cpp
+++ floyedWarshell.cpp
@@ -15,5 +15,24 @@ int main(){
         cin>>u>>v>>c;
         adj[u][v]=c;
     }
+    //path relaxation
+    for(int k=0; k<n; k++){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(adj[i][k]+adj[j][k]<adj[i][j]){
+                    adj[i][j]=adj[i][k]+adj[j][k]
+                }
+    }
+        
+    }
+    }
+    //cycle detection;
+    //adjMatrix er primary diagonal cell er kuno ekti cell er value zodi 0 theke chuto hoy tahole graph e cycle ache.
+    for(int i=0; i<n; i++){
+        //adj[i][i] primary diagonal cell;
+        if(adj[i][i]<0){
+            cout<<"cycle detected"<<endl;
+        }
+    }
     return 0;
 }
