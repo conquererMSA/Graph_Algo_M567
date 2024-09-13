653a9362ab09b3a1fe25f7cd40e53bcd608a2e1c
asus test <msa-99@outlook.com>
2024-09-13 12:25:23 +0600

    path relaxed

--- bellmanFord.cpp
+++ bellmanFord.cpp
@@ -9,6 +9,7 @@ class Edge{
         this->c=c;
     }
 };
+int dis[100];
 int main(){
     int n,e;
     cin>>n>>e;
@@ -18,8 +19,25 @@ int main(){
         cin>>u>>v>>c;
         edges.push_back(Edge(u,v,c));
     }
-    for(int i=0; i<e; i++){
-        cout<<edges[i].u<<" "<<edges[i].v<<" "<<edges[i].c<<endl;
+    for(int i=0; i<n; i++){
+        dis[i]=INT_MAX;
+    }
+    dis[0]=0; //set source cost
+    for(int i=0; i<n; i++){
+        for(Edge ed:edges){
+        int u,v,c;
+        u=ed.u;
+        v=ed.v;
+        c=ed.c;
+        // dis[u]<INT_MAX hole ei node e visit kora hoyeche end er cost set ache
+        if(dis[u]<INT_MAX&& dis[u]+c<dis[v]){
+            dis[v]=dis[u]+c;
+        }
+    }
+
+    }
+    for(int i=0; i<n; i++){
+     cout<<i<<"-> "<<dis[i]<<endl;
     }
     return 0;
 }
